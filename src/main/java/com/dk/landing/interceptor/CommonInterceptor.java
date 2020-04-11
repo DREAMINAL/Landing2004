31c5aa95621b759b4c8af53067d14b5ec0eb1425
DK <dread_k@naver.com>
2020-04-11 19:31:59 +0900

    Intercepter 추가 Logback 파일로 내릴 수 있도록 기본설정 후 주석처리 Unstaged Changes 안넣어서 추가 Commit

---
+++ src/main/java/com/dk/landing/interceptor/CommonInterceptor.java
@@ -0,0 +1,31 @@
+package com.dk.landing.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class CommonInterceptor extends HandlerInterceptorAdapter {
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.info("Interceptor > preHandle");
+		return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("Interceptor > postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) throws Exception {
+        log.info("Interceptor > afterCompletion" );
+    }
+}
